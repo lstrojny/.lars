f48f43951942283c11251cc79c9348903fac61e7
root <root@in01.local.internations.org>
2013-02-17 21:30:37 +0100

    Fixing base dir issues

--- install.sh
+++ install.sh
@@ -1,20 +1,22 @@
 #!/bin/sh
-git pull
+base=`dirname $0`
+
+(cd $base && git pull; cd -)
 
 echo "Installing .bashrc"
-ln -sf $PWD/dot-bashrc ~/.bashrc
+ln -sf $base/dot-bashrc ~/.bashrc
 
 echo "Installing .bash_profile"
-ln -sf $PWD/dot-bashrc ~/.bash_profile
+ln -sf $base/dot-bashrc ~/.bash_profile
 
 echo "Installing .caprc"
-ln -sf $PWD/dot-caprc ~/.caprc
+ln -sf $base/dot-caprc ~/.caprc
 
 echo "Installing .gitconfig"
-ln -sf $PWD/dot-gitconfig ~/.gitconfig
+ln -sf $base/dot-gitconfig ~/.gitconfig
 
 echo "Installing .vimrc"
-ln -sf $PWD/dot-vimrc ~/.vimrc
+ln -sf $base/dot-vimrc ~/.vimrc
 
 echo "Install vundle"
 [ -d ~/.vim/bundle/vundle ] && (cd ~/.vim/bundle/vundle && git pull) || git clone -q https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
