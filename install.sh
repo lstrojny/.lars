84b717883c6c246c5b944a76094f510fade7daa4
Lars Strojny <lars@strojny.net>
2013-02-17 15:25:55 +0100

    Switching to vundle

--- install.sh
+++ install.sh
@@ -1,14 +1,4 @@
 #!/bin/sh
-
-install_vimext() {
-    for dir in $1/*; do
-        if [ -d $dir ]; then
-            mkdir -p ~/.vim/`basename $dir`
-            rsync -r --exclude="*/.git/*" --exclude="*/.svn/*" $dir/* ~/.vim/`basename $dir`
-        fi
-    done
-}
-
 echo "Installing .bashrc"
 ln -sf $PWD/dot-bashrc ~/.bashrc
 
@@ -24,28 +14,8 @@ ln -sf $PWD/dot-gitconfig ~/.gitconfig
 echo "Installing .vimrc"
 ln -sf $PWD/dot-vimrc ~/.vimrc
 
-rm -rf temp
-mkdir temp
-echo "Installing Erlang support for VIM"
-git clone -q git://github.com/oscarh/vimerl.git temp/vimerl
-install_vimext temp/vimerl
+echo "Install vundle"
+[ -d ~/.vim/bundle/vundle ] && (cd ~/.vim/bundle/vundle && git pull) || git clone -q https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 
-echo "Installing Scala support for VIM"
-svn -q checkout http://lampsvn.epfl.ch/svn-repos/scala/scala-tool-support/trunk/src/vim/ temp/scala
-install_vimext temp/scala
-
-echo "Installing Textile support for VIM"
-wget -q "http://www.vim.org/scripts/download_script.php?src_id=9427" -O temp/textile.zip
-unzip -q temp/textile.zip -d temp
-install_vimext temp/textile
-
-echo "Installing PHP folding support for VIM"
-wget -q "http://www.vim.org/scripts/download_script.php?src_id=12114" -O ~/.vim/plugin/phpfolding.vim
-
-echo "Installing Twig support for VIM"
-git clone -q git://github.com/beyondwords/vim-twig.git temp/vim-twig
-install_vimext temp/vim-twig
-
-echo "Installing coffee-script support for VIM"
-git clone -q git://github.com/kchmck/vim-coffee-script.git temp/vim-coffee-script
-install_vimext temp/vim-coffee-script
+echo "Run vundle install"
+vim +BundleInstall +qall
